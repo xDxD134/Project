c57803e6e52f9cd1fc9eaa2207f981707bc4cad0
Ilya <ilya.mishustin.98@mail.ru>
2023-04-01 12:40:53 +0300

    условия

---
+++ js/script8.js
@@ -0,0 +1,53 @@
+"use strist";
+
+if (4 == 9) {                   // пример условия. В консоле ничего не будет выводиться, т.к. условие неверное 
+  console.log("Ok");
+}   
+
+if (9 == 9) {                   // условия верное в консоле выведется ОК
+  console.log("Ok");
+}
+
+if (4 == 9) {
+    console.log('Ok!');
+} else {                        // пример else. Else используется для того чтобы произошло дествие при неверном условии 
+    console.log('Error');
+}
+
+                                    // УСЛОВИЯ БЫВАЕТ МНОГО
+
+const num = 50;
+
+if (num < 49) {
+   console.log('Error');
+} else if (num > 100) {
+   console.log ('Много');
+} else {
+   console.log("Ok!");
+}
+
+                                    // СИНТАКСИС ЗАПИСИ УСЛОВИЙ ЧЕРЕЗ ТЕРНАРНЫЙ ОПЕРАТОР
+
+const Num = 50;
+
+(Num == 50) ? console.log("Ok!") : console.log("Error");
+
+                                    // ПРИМЕР Конструкции switch 
+
+const Numb = 50;
+
+switch (Numb) {
+    case 49:
+        console.log("Неверно");
+        break;
+    case 100:
+        console.log("Много");
+        break;
+    case 50:
+        console.log("В точку");
+        break;
+    default:                                // Default будет работать если ни один из кейсов неверный
+        console.log("Не в этот раз");
+        break;
+}
+
