daecb589fa318d739e4cb852ec3fb9936d4e977e
Ilya <ilya.mishustin.98@mail.ru>
2023-04-01 11:29:58 +0300

    ConsoleJava

--- js/script7.js
+++ js/script7.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false    
+};
+
+  const  a = prompt("Один из последних просмотренных фильмов", ""),
+         b = prompt("Насолько оцените его?", ""),
+         c = prompt("Один из последних просмотренных фильмов", ""),
+         d = prompt("Насолько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
